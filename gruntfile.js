557742fbbe90bd4d7f0df2ff0274a62775662ecf
Trond Brynhildsvoll Tenfjord <trond.tenfjord@gmail.com>
2016-12-07 18:08:20 +0100

    First commit

---
+++ gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json'],
+        commitFiles: ['package.json', 'bower.json'],
+        tagMessage: 'v%VERSION%',
+      },
+      release: {
+
+      },
+      snapshot: {
+        options: {
+          metadata: 'SNAPSHOT',
+          createTag: false,
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-bump');
+
+  //var dry = grunt.option()
+  //grunt.registerTask('release', ['bump-only --dry-run']);
+
+};
